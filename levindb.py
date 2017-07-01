e644d7a987441defe6def3727c67b20801f1a4be
Philip Winter <pwinter@ualberta.ca>
2017-07-01 13:16:30 -0600

    Added code for Arbabimo project

--- levindb.py
+++ levindb.py
@@ -1,6 +1,7 @@
 import sqlite3
 import csv
 import go_obo_parser
+import os
 
 DB_FILENAME = 'levin.db'
 OBO_FILENAME = 'data/bto/BrendaTissueOBO.obo'
@@ -324,7 +325,6 @@ def cleanup_ontology():
     conn.close()
 
 def setup_biogps():
-    add_db('biogps', 'http://biogps.org/')
     translation_f = open(BIOGPS_TRANSLATION_TABLE, 'rU')
     translation_r = csv.reader(translation_f)
     translation_r.next()
@@ -341,7 +341,7 @@ def cleanup_dbtissue():
     conn.commit()
     conn.close()
 
-def setup_proteins():
+def setup_ion_channels():
     prot_file = open(PROTEIN_FILENAME, 'rU')
     prot_reader = csv.reader(prot_file)
     prot_reader.next()
@@ -367,6 +367,29 @@ def setup_proteins():
         if check_gene_symbol != '':
             continue
         add_protein(uniprot_accnum, gene_symbol, name, process_function)
+    prot_file.close()
+
+def setup_all_proteins():
+    mapping_dict = {}
+    uniprot_symbol_file = open('data/uniprot/HUMAN_9606_idmapping_Gene_Name.dat')
+    for line in uniprot_symbol_file:
+        uniprot_accum = line.strip().split()[0]
+        gene_symbol = line.strip().split()[2]
+        mapping_dict[gene_symbol] = uniprot_accum
+    uniprot_symbol_file.close()
+    prot_file = open('data/biogps/gnf1h.annot2007.tsv', 'rU')
+    prot_reader = csv.reader(prot_file, delimiter='\t')
+    prot_reader.next()
+    for row in prot_reader:
+        gene_symbol = row[6]
+        if not gene_symbol in mapping_dict:
+            'Could not find uniprot accession for %s' % gene_symbol
+            continue
+        uniprot_accum = mapping_dict[gene_symbol]
+        if exists_protein(uniprot_accum):
+            continue
+        add_protein(uniprot_accum, gene_symbol, '', '')
+        print 'Added %s %s' % (uniprot_accum, gene_symbol)
     prot_file.close()
 
 def cleanup_proteins():
@@ -424,12 +447,12 @@ def setup_chembl():
                 approval_status = 'UNKNOWN'
             add_compound('', '', chembl_compound_id, compound_name_in_doc,
                     approval_status, first_approval, 'chembl')
-            print 'Added compound %s' % chembl_compound_id
+            #print 'Added compound %s' % chembl_compound_id
         if exists_protein(uniprot) and exists_compound(chembl_compound_id):
             compound_id = lookup_compound_by_name(chembl_compound_id)
             if not exists_interaction(uniprot, compound_id):
                 add_interaction(uniprot, compound_id, sourcedb_name='chembl')
-                print 'Added interaction %s %s' % (uniprot, chembl_compound_id)
+                #print 'Added interaction %s %s' % (uniprot, chembl_compound_id)
     chembl_compound_file.close()
 
 def setup_hmdb():
@@ -496,10 +519,10 @@ def process_biogps_microarray(annot_dict):
         for i in xrange(0, len(tissue_list)):
             expr_level = float(row[i + 1])
             tissue_name = lookup_dbtissue('biogps', tissue_list[i])
-            print tissue_name
             add_expression(tissue_name, uniprot_accnum, expr_level, 
                     assay_type='microarray', dataset_name=BIOGPS_DATASET, 
                     sourcedb_name='biogps')
+            #print 'Added %s %s' % (uniprot_accnum, tissue_name)
     microarray_f.close()
 
 def process_biogps_data():
@@ -513,4 +536,101 @@ def cleanup_expression():
     conn.commit()
     conn.close()
 
+def get_expression_for_tissue(tissue_name):
+    prot_expr_list = []
+    conn = sqlite3.connect(DB_FILENAME)
+    curs = conn.cursor()
+    for row in curs.execute('''SELECT ProteinUniProtAccNum, ExprLevel
+            FROM Expression WHERE TissueName = ?''',
+            (tissue_name,)):
+        prot_expr_list.append((row[0], row[1]))
+    conn.commit()
+    conn.close()
+    return prot_expr_list
+
+def normalize_prot_expr_list(prot_expr_list):
+    normalized_list = []
+    total_expr = 0.0
+    for elem in prot_expr_list:
+        upac = elem[0]
+        expr = elem[1]
+        total_expr += expr
+    for elem in prot_expr_list:
+        upac = elem[0]
+        expr = elem[1]
+        norm_expr = expr / total_expr
+        normalized_list.append((upac, norm_expr))
+    return normalized_list
+
+def get_uniprot_seq(upac):
+    ls = os.listdir('data/seq')
+    fn = '%s.fasta' % upac
+    if fn == 'Q96LR1.fasta':
+        return ''
+    if not fn in ls:
+        cmd = 'wget -O data/seq/%s http://www.uniprot.org/uniprot/%s' % (fn, fn)
+        #print cmd
+        os.system(cmd)
+    f = open('data/seq/%s' % fn)
+    f.next()
+    seqlist = []
+    for line in f:
+        seqlist.append(line.strip())
+    f.close()
+    seq = ''.join(seqlist)
+    return seq
+
+def get_expression_prob_for_all_tissues():
+    aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
+            'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+    out_file = open('aa_out.csv', 'w')
+    out_file.write('tissue')
+    for aa in aa_list:
+        out_file.write(',%s' % aa)
+    out_file.write('\n')
+    tissue_list = []
+    conn = sqlite3.connect(DB_FILENAME)
+    curs = conn.cursor()
+    for row in curs.execute('''SELECT Name FROM Tissue'''):
+        tissue_list.append(row[0])
+    conn.commit()
+    conn.close()
+    data_dict = {}
+    count = 0
+    for tissue in tissue_list:
+        #if count == 1:
+        #    break
+        prot_expr_list = get_expression_for_tissue(tissue)
+        if len(prot_expr_list) > 0:
+            print 'Normalizing %s' % tissue
+            data_dict[tissue] = normalize_prot_expr_list(prot_expr_list)
+            count += 1
+    for tissue in data_dict:
+        print 'Probablizing %s' % tissue
+        overall_aa_prob = {}
+        normalized_list = data_dict[tissue]
+        for elem in normalized_list:
+            upac, expr = elem
+            seq = get_uniprot_seq(upac)
+            seq_freq_dict = {}
+            total_aa = 0.0
+            for aa in seq:
+                total_aa += 1
+                if aa in seq_freq_dict:
+                    seq_freq_dict[aa] += 1.0
+                else:
+                    seq_freq_dict[aa] = 1.0
+            seq_prob_dict = {}
+            for aa in seq_freq_dict:
+                prob = seq_freq_dict[aa] / total_aa
+                seq_prob_dict[aa] = prob
+                if aa in overall_aa_prob:
+                    overall_aa_prob[aa] += expr * prob
+                else:
+                    overall_aa_prob[aa] = expr * prob
+        out_file.write('%s' % tissue)
+        for aa in aa_list:
+            out_file.write(',%.6f' % overall_aa_prob[aa])
+        out_file.write('\n')
+    out_file.close()
 
--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#rm levin.db
+rm levin.db
 python -c "from levindb import *;create_tables()"
 python -c "from levindb import *;cleanup_externaldb()"
 python -c "from levindb import *;setup_externaldb()"
 python -c "from levindb import *;cleanup_ontology()"
 python -c "from levindb import *;setup_ontology()"
 python -c "from levindb import *;cleanup_proteins()"
-python -c "from levindb import *;setup_proteins()"
+python -c "from levindb import *;setup_ion_channels()"
+#python -c "from levindb import *;setup_all_proteins()"
 python -c "from levindb import *;cleanup_dbtissue()"
 python -c "from levindb import *;setup_biogps()"
 python -c "from levindb import *;cleanup_expression()"
@@ -14,3 +15,4 @@ python -c "from levindb import *;process_biogps_data()"
 python -c "from levindb import *;cleanup_compounds()"
 python -c "from levindb import *;setup_chembl()"
 python -c "from levindb import *;dump_database()"
+#python -c "from levindb import *;get_expression_prob_for_all_tissues()"
